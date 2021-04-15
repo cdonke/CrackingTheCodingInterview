1a48fb40a472ae1fb65f0cf08eab00d6923fd7af
Christiano Donke <cdonke@outlook.com>
2021-04-15 17:50:05 -0300

    Initial commit

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace CrackingTheCodingInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+                BenchmarkRunner.Run(typeof(Program).Assembly);
+            else
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+        }
+    }
+}
