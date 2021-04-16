e2c0c1a053e6626fb16341015df9572b0b3df06a
Christiano Donke <cdonke@outlook.com>
2021-04-16 19:44:14 -0300

    My ArrayList

---
+++ CrackingTheCodingInterview.Tests/DataStructures/ArrayList/ArrayListTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace CrackingTheCodingInterview.Tests.DataStructures.ArrayList
+{
+    [TestClass]
+    public class ArrayListTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void Add(int[] items, int capacity)
+        {
+            var arrayList = new CrackingTheCodingInterview.DataStructures.ArrayList.MyArrayList<int>(capacity);
+            foreach (var item in items)
+                arrayList.Add(item);
+
+            Assert.AreEqual(items.Length, arrayList.Count);
+        }
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void AddRange_Array(int[] items, int capacity)
+        {
+            var arrayList = new CrackingTheCodingInterview.DataStructures.ArrayList.MyArrayList<int>(capacity);
+            arrayList.AddRange(items);
+
+            Assert.AreEqual(items.Length, arrayList.Count);
+        }
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void AddRange_List(IEnumerable<int> items, int capacity)
+        {
+            var arrayList = new CrackingTheCodingInterview.DataStructures.ArrayList.MyArrayList<int>(capacity);
+            arrayList.AddRange(items);
+
+            Assert.AreEqual(items.Count(), arrayList.Count);
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void Clear(int[] items, int capacity)
+        {
+            var arrayList = new CrackingTheCodingInterview.DataStructures.ArrayList.MyArrayList<int>();
+            arrayList.AddRange(items);
+            arrayList.Clear();
+
+            Assert.AreEqual(0, arrayList.Count);
+        }
+
+
+        public static IEnumerable<object[]> Data()
+        {
+            yield return new object[] { Enumerable.Range(0, 10).ToArray(), 100 };
+            yield return new object[] { Enumerable.Range(0, 100).ToArray(), 100 };
+            yield return new object[] { Enumerable.Range(0, 1000).ToArray(), 100 };
+            yield return new object[] { Enumerable.Range(0, 10000).ToArray(), 1000 };
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/DataStructures/ArrayList/MyArrayList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingTheCodingInterview.DataStructures.ArrayList
+{
+    public class MyArrayList<T>
+    {
+        private readonly int _factor;
+        private int _capacity;
+        private T[] _collection;
+        private int _count;
+
+        public MyArrayList() : this(1000, 2) { }
+        public MyArrayList(int capacity) : this(capacity, 2) { }
+        public MyArrayList(int capacity, int factor)
+        {
+            _factor = factor;
+            _capacity = capacity;
+            _collection = new T[_capacity];
+        }
+
+        public int Count
+            => _count;
+
+
+        public void Add(T item)
+        {
+            _collection[_count++] = item;
+            if (_count == _capacity)
+            {
+                Resize();
+            }
+        }
+
+        public void AddRange(IEnumerable<T> items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+        public void AddRange(T[] items)
+        {
+            foreach (var item in items)
+            {
+                Add(item);
+            }
+        }
+        public void Clear()
+        {
+            for (; _count > 0; _count--)
+            {
+                _collection[_count] = default(T);
+            }
+        }
+
+        private void Resize()
+        {
+            var capacity = _capacity * _factor;
+            var collection = new T[capacity];
+
+            for (int i = 0; i < _count; i++)
+            {
+                collection[i] = _collection[i];
+            }
+
+            _capacity = capacity;
+            _collection = collection;
+        }
+    }
+}
