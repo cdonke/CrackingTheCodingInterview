e7e1504691f60f4861779ec85e42d0494b02d881
Christiano Donke <cdonke@outlook.com>
2021-04-19 12:30:49 -0300

    Process debugging argument

--- CrackingTheCodingInterview/Program.cs
+++ CrackingTheCodingInterview/Program.cs
@@ -12,7 +12,11 @@ namespace CrackingTheCodingInterview
             if (args.Length > 0)
                 BenchmarkRunner.Run(typeof(Program).Assembly);
             else
+#if DEBUG
                 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+#else
+                BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+#endif
         }
     }
 }
