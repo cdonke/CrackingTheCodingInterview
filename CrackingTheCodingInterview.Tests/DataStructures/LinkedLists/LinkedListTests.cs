e4efe647296ea6633c91a14a3eb8efec27e6ded0
Christiano Donke <cdonke@outlook.com>
2021-05-03 16:14:51 -0300

    Linked List

---
+++ CrackingTheCodingInterview.Tests/DataStructures/LinkedLists/LinkedListTests.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NuGet.Frameworks;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CrackingTheCodingInterview.Tests.DataStructures.LinkedLists
+{
+    [TestClass]
+    public class LinkedListTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 })]
+        public void Add(int[] data)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            Assert.AreEqual(data.Length, ll.Count);
+            for (int i = 0; i < data.Length; i++)
+            {
+                Assert.AreEqual(data[i], ll[i]);
+            }
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 2, new int[] { 1, 2, 3, 4, 5 })]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 0, new int[] { 3, 1, 2, 4, 5 })]
+        public void InsertAt(int[] data, int number, int position, int[] expected)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            ll.InsertAt(number, position);
+
+            Assert.AreEqual(expected.Length, ll.Count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], ll[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 4)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 5)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 6)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, -1)]
+        public void InsertAt_Exception(int[] data, int number, int position)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            ll.InsertAt(number, position);
+        }
+
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 3, 0, 2)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 30, 0, -1)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 3 }, 3, 5, 8)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 3 }, 3, 0, 2)]
+        public void IndexOf(int[] data, int number, int start, int expected)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            var actual = ll.IndexOf(start, number);
+
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 4)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 5)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 6)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, -1)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3, 16)]
+        public void IndexOf_Exception(int[] data, int number, int start)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            var actual = ll.IndexOf(start, number);
+        }
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 2, new int[] { 1, 2, 4, 5 })]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 0, new int[] { 2, 3, 4, 5 })]
+        public void Delete(int[] data, int toDelete, int[] expected)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            ll.Delete(toDelete);
+
+            Assert.AreEqual(expected.Length, ll.Count);
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.AreEqual(expected[i], ll[i]);
+            }
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 5)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, 6)]
+        [DataRow(new int[] { 1, 2, 3, 4, 5 }, -1)]
+        public void Delete_Exception(int[] data, int toDelete)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            ll.Delete(toDelete);
+        }
+
+
+        [TestMethod]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 3)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 0)]
+        public void Indexer(int[] data, int idx)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            var actual = ll[idx];
+
+            Assert.AreEqual(data[idx], actual);
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        [DataRow(new int[] { 1, 2, 4, 5 }, -1)]
+        [DataRow(new int[] { 1, 2, 4, 5 }, 10)]
+        public void Indexer_Exception(int[] data, int idx)
+        {
+            var ll = new CrackingTheCodingInterview.DataStructures.LinkedLists.LinkedList<int>();
+            foreach (var item in data)
+            {
+                ll.Add(item);
+            }
+
+            var actual = ll[idx];
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/DataStructures/LinkedLists/LinkedList.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CrackingTheCodingInterview.DataStructures.LinkedLists
+{
+    public class LinkedList<T>
+    {
+        [DebuggerStepThrough]
+        [DebuggerDisplay("{value}")]
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.value = value;
+            }
+
+            public T value { get; set; }
+            public Node next { get; set; }
+        }
+
+        private Node _head;
+
+        public LinkedList()
+        { }
+
+        public T this[int element]
+        {
+            get
+            {
+                if (element < 0 || element >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                Node current = _head;
+                if (element == 0)
+                {
+                    return current.value;
+                }
+
+
+                for (int i = 0; i < element; i++)
+                {
+                    current = current.next;
+                }
+
+
+
+                return current.value;
+            }
+        }
+        public int Count { get; private set; }
+
+        public void Add(T value)
+        {
+            var node = new Node(value);
+            if (_head == null)
+            {
+                _head = node;
+            }
+            else
+            {
+                var current = _head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = node;
+            }
+
+            Count++;
+        }
+        public void InsertAt(T value, int position)
+        {
+            if (position < 0 || position >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            var newNode = new Node(value);
+
+            if (position == 0)
+            {
+                newNode.next = _head;
+                _head = newNode;
+            }
+            else
+            {
+                Node previous = _head,
+                    current = _head.next;
+
+                for (int i = 1; i < position; i++)
+                {
+                    previous = current;
+                    current = current.next;
+                }
+
+                newNode.next = current;
+                previous.next = newNode;
+            }
+            Count++;
+        }
+
+        public int IndexOf(int start, T value)
+        {
+            if (start < 0 || start >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+
+            Node node = _head;
+            int i;
+            for (i = 0; i < Count; i++)
+            {
+                if (i < start)
+                {
+                    node = node.next;
+                    continue;
+                }
+
+                if (node.value.Equals(value))
+                {
+                    return i;
+                }
+                else
+                {
+                    node = node.next;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Delete(int element)
+        {
+            if (element < 0 || element >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            if (element == 0)
+            {
+                _head = _head.next;
+            }
+            else
+            {
+                Node previous = _head,
+                    current = _head.next;
+
+                for (int i = 1; i < element; i++)
+                {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+            }
+
+            Count--;
+        }
+
+    }
+}
