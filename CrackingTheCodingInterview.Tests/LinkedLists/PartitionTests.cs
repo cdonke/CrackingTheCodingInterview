1ef1f49c346577d461b64a02342381cbdad0b26a
Christiano Donke <cdonke@outlook.com>
2021-05-05 00:12:58 -0300

    Linked List kth to last and partition #notests

---
+++ CrackingTheCodingInterview.Tests/LinkedLists/PartitionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterview.Tests.LinkedLists
+{
+    [TestClass]
+    public class PartitionTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(Data), DynamicDataSourceType.Method)]
+        public void FirstTry(LinkedList<int> list, int partition, LinkedList<int> expected)
+        {
+            var solution = new CrackingTheCodingInterview.LinkedLists.Partition.Partition();
+            var actual = solution.FirstTry(list, partition, null);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        public static IEnumerable<object[]> Data()
+            => CrackingTheCodingInterview.LinkedLists.Partition.Partition.Data();
+        
+    }
+}
---
+++ CrackingTheCodingInterview/LinkedLists/KthLast/KthLast.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview.LinkedLists.KthLast
+{
+    public class KthLast : BenchmarkBase
+    {
+        public static IEnumerable<object[]> Data()
+        {
+            yield return new object[] { new LinkedList<int>(new[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 }), 1, 1 };
+            yield return new object[] { new LinkedList<int>(new[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 }), 4, 4 };
+            yield return new object[] { new LinkedList<int>(new[] { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 }), 2, 2 };
+        }
+
+        public int FirstTry(LinkedList<int> list, int kth, int expected)
+        {
+            var node = list.First;
+            var kthElement = list.Count - kth;
+            for (int i = 0; i < kthElement; i++)
+            {
+                node = node.Next;
+            }
+
+            return node.Value;
+        }
+
+        // Thinking if a scenario where I don't have the LinkedList Count
+        public int SecondTry(LinkedList<int> list, int kth, int expected)
+        {
+            var node = list.First;
+            for (int i = 0; i < kth; i++)
+            {
+                node = node.Next;
+            }
+
+            LinkedListNode<int> kthElement = list.First;
+
+            while(node != null)
+            {
+                kthElement = kthElement.Next;
+                node = node.Next;
+            }
+
+            return kthElement.Value;
+        }
+    }
+}
