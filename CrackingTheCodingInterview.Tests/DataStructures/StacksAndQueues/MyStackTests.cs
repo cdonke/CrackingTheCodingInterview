c9709fe1ea2e2f013e720014145eb7958277421a
Christiano Donke <cdonke@outlook.com>
2021-05-06 17:12:02 -0300

    Stacks

---
+++ CrackingTheCodingInterview.Tests/DataStructures/StacksAndQueues/MyStackTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace CrackingTheCodingInterview.Tests.DataStructures.StacksAndQueues
+{
+    [TestClass]
+    public class MyStackTests
+    {
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 10)]
+        public void Add(int[] data, int expectedCount)
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            foreach (var item in data)
+            {
+                stack.Push(item);
+            }
+
+            Assert.AreEqual(expectedCount, stack.Count);
+            for (int i = data.Length - 1; i >= 0; i--)
+            {
+                Assert.AreEqual(data[i], stack.Pop());
+            }
+        }
+
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 10)]
+        public void Pop(int[] data, int expectedCount)
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            foreach (var item in data)
+            {
+                stack.Push(item);
+            }
+
+            Assert.AreEqual(expectedCount, stack.Count);
+            stack.Pop();
+            stack.Pop();
+            stack.Pop();
+
+            Assert.AreEqual(expectedCount - 3, stack.Count);
+        }
+
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, 0)]
+        [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7 }, 7)]
+        public void Peek(int[] data, int expected)
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            foreach (var item in data)
+            {
+                stack.Push(item);
+            }
+
+            var actual = stack.Peek();
+            Assert.AreEqual(data.Length, stack.Count);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [DataRow(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, false)]
+        [DataRow(new int[0], true)]
+        public void IsEmpty(int[] data, bool expected)
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            foreach (var item in data)
+            {
+                stack.Push(item);
+            }
+
+            Assert.AreEqual(expected, stack.IsEmpty);
+        }
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(CrackingTheCodingInterview.DataStructures.StacksAndQueues.StackEmptyException))]
+        public void Peek_Exception()
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            stack.Peek();
+        }
+
+
+        [TestMethod]
+        [ExcludeFromCodeCoverage]
+        [ExpectedException(typeof(CrackingTheCodingInterview.DataStructures.StacksAndQueues.StackEmptyException))]
+        public void Pop_Exception()
+        {
+            var stack = new CrackingTheCodingInterview.DataStructures.StacksAndQueues.MyStack<int>();
+            stack.Pop();
+        }
+    }
+}
---
+++ CrackingTheCodingInterview/DataStructures/StacksAndQueues/MyStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CrackingTheCodingInterview.DataStructures.StacksAndQueues
+{
+    public class StackEmptyException : Exception { }
+    public class MyStack<T>
+    {
+        private class StackNode
+        {
+            public StackNode(T value)
+            {
+                this.value = value;
+            }
+
+            public T value { get; set; }
+            public StackNode next { get; set; }
+        }
+        
+        private StackNode _top;
+
+        public int Count { get; private set; }
+
+        public void Push(T value)
+        {
+            var node = new StackNode(value);
+            node.next = _top;
+            _top = node;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (IsEmpty)
+            {
+                throw new StackEmptyException();
+            }
+
+            T value = _top.value;
+            _top = _top.next;
+
+            Count--;
+            return value;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty)
+            {
+                throw new StackEmptyException();
+            }
+
+            return _top.value;
+        }
+
+        public bool IsEmpty => _top == null;
+    }
+}
