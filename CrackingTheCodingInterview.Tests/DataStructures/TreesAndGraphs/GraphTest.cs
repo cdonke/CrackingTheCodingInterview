be5219cd28a552532d17e5b2f36aa2e0c3e57fac
Christiano Donke <cdonke@outlook.com>
2021-05-23 19:26:51 -0300

    Graphs

---
+++ CrackingTheCodingInterview.Tests/DataStructures/TreesAndGraphs/GraphTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingTheCodingInterview.Tests.DataStructures.TreesAndGraphs
+{
+    [TestClass]
+    public class GraphTest
+    {
+        [TestMethod]
+        public void UnidirecionalTests()
+        {
+            var solution = new CrackingTheCodingInterview.DataStructures.TreesAndGraphs.Graph();
+            solution.AddEdge(6, 5, false);
+            solution.AddEdge(6, 3, false);
+            solution.AddEdge(5, 4, false);
+            solution.AddEdge(4, 6, false);
+        }
+
+        [TestMethod]
+        public void BidiretionalTests()
+        {
+            var solution = new CrackingTheCodingInterview.DataStructures.TreesAndGraphs.Graph();
+            solution.AddEdge(6, 5, true);
+            solution.AddEdge(6, 3, true);
+            solution.AddEdge(5, 4, true);
+            solution.AddEdge(4, 6, true);
+        }
+
+        [TestMethod]
+        public void MixedTests()
+        {
+            var solution = new CrackingTheCodingInterview.DataStructures.TreesAndGraphs.Graph();
+            solution.AddEdge(0, 1, false);
+            solution.AddEdge(1, 2, false);
+            solution.AddEdge(2, 0, false);
+            solution.AddEdge(2, 3, false);
+        }
+
+        [TestMethod]
+        public void DFS_Unidirecional()
+        {
+            var solution = new CrackingTheCodingInterview.DataStructures.TreesAndGraphs.Graph();
+            solution.AddEdge(0, 1, false);
+            solution.AddEdge(0, 4, false);
+            solution.AddEdge(0, 5, false);
+            solution.AddEdge(1, 3, false);
+            solution.AddEdge(1, 4, false);
+            solution.AddEdge(3, 4, false);
+            solution.AddEdge(3, 2, false);
+
+            var expected = new[] { 0, 1, 3, 4, 2, 5 };
+
+            var actual = solution.DepthFirstSearch(0);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void DFSBidirecional()
+        {
+            var solution = new CrackingTheCodingInterview.DataStructures.TreesAndGraphs.Graph();
+            solution.AddEdge(0, 1, false);
+            solution.AddEdge(0, 4, false);
+            solution.AddEdge(0, 5, false);
+            solution.AddEdge(1, 3, true);
+            solution.AddEdge(1, 4, false);
+            solution.AddEdge(3, 4, true);
+            solution.AddEdge(3, 2, false);
+
+            var expected = new[] { 0, 1, 3, 4, 2, 5 };
+
+            var actual = solution.DepthFirstSearch(0);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ CrackingTheCodingInterview/DataStructures/TreesAndGraphs/Graph.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CrackingTheCodingInterview.DataStructures.TreesAndGraphs
+{
+    
+
+    public class Graph
+    {
+        private Dictionary<int, int[]> _adjacentList = new Dictionary<int, int[]>();
+        private List<int> _dfsResult = new List<int>();
+
+        public void AddVertex(int value)
+        {
+            _adjacentList.Add(value, null);
+        }
+        public void AddEdge(int left, int right, bool bidirectional)
+        {
+            AddItem(left, right);
+
+            if (bidirectional)
+            {
+                AddItem(right, left);
+            }
+        }
+        public int[] GetVertexes(int node)
+        {
+            return _adjacentList[node];
+        }
+
+        private void AddItem(int node1, int node2)
+        {
+            if (!_adjacentList.ContainsKey(node1))
+            {
+                AddVertex(node1);
+            }
+
+            var value = _adjacentList[node1] as int[];
+            if (value == null)
+            {
+                _adjacentList[node1] = new int[1] { node2 };
+                return;
+            }
+
+            var newValue = new int[value.Length + 1];
+            value.CopyTo(newValue, 0);
+            newValue[value.Length] = node2;
+
+            _adjacentList[node1] = newValue;
+        }
+
+
+        public int[] DepthFirstSearch(int root)
+        {
+            _dfsResult.Clear();
+
+            if (!_adjacentList.ContainsKey(root))
+            {
+                return _dfsResult.ToArray();
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            visited.Add(root);
+
+            _dfsResult.Add(root);
+            Visit(root, visited);
+
+            return _dfsResult.ToArray();
+        }
+        private void Visit(int node, HashSet<int> visited)
+        {
+            if (!_adjacentList.ContainsKey(node))
+            {
+                return;
+            }
+
+
+            var children = _adjacentList[node];
+            foreach (var child in children)
+            {
+                if (!visited.Contains(child))
+                {
+                    _dfsResult.Add(child);
+                    visited.Add(child);
+                    Visit(child, visited);
+                }
+            }
+
+        }
+    }
+}
