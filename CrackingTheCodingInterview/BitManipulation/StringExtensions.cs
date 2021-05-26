62dec7487d73e4e3b5886ddf435f6eeef154c8f3
Christiano Donke <cdonke@outlook.com>
2021-05-25 21:02:05 -0300

    Insertion

---
+++ CrackingTheCodingInterview/BitManipulation/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CrackingTheCodingInterview.BitManipulation
+{
+    public static class StringExtensions
+    {
+        public static int ToBinary (this string S)
+        {
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (S[i] != '0' && S[i] != '1')
+                {
+                    throw new InvalidCastException("Not a binary notarion");
+                }
+            }
+
+            return Convert.ToInt32(S, 2);
+        }
+    }
+}
